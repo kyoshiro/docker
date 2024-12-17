4893923ee8dbea124c4e5e1e2bc1e4ba30a6ab69
kyoshiro <108440+kyoshiro@users.noreply.github.com>
2024-12-17 21:09:29 +0100

    switch compose files to podman

--- gitlab/start_gitlab_runner.sh
+++ gitlab/start_gitlab_runner.sh
@@ -1,16 +1,16 @@
 #!/bin/bash
-export BASE_PATH=/srv/docker/data
+export BASE_PATH=/srv/containers/data
 
 start_gitlab_runner() {
 	local GITLAB_RUNNER_NAME="$1"
 
 	echo "Starting gitlab-runner $GITLAB_RUNNER_NAME"
 
-	docker run -d --name $GITLAB_RUNNER_NAME --restart always \
+	podman run -d --name $GITLAB_RUNNER_NAME --restart always \
 	 --add-host gitlab.iot.intern:172.16.1.226 \
          -v $BASE_PATH/$GITLAB_RUNNER_NAME/config:/etc/gitlab-runner \
          -v $BASE_PATH/$GITLAB_RUNNER_NAME/certs:/etc/gitlab-runner/certs \
-         -v /var/run/docker.sock:/var/run/docker.sock \
+         -v /run/user/1001/podman/podman.sock:/run/podman/podman.sock \
          gitlab/gitlab-runner:alpine
 }
 
